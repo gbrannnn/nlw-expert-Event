cab924cadbf7c6f07f93f4986b052f77ba4212ff
Gabriel Brandão Vechani de Freitas <114454118+gbrannnn@users.noreply.github.com>
2024-02-12 23:58:11 -0300

    Primeira parte da API
    Iniciamos o desenvolvimento de uma API, apenas conectando um DBlite e coletando suas infomações.

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auction.GetCurrent;
+
+namespace RocketseatAuction.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetCurreentAuction()
+    {
+        var useCase = new GetCurrentAuctionUseCase();
+
+        var result = useCase.Execute();
+
+        if (result is null)
+            return NoContent();
+
+        return Ok(result);
+    }
+}
